4e71862b7baa285e0070f386ad35b10282923eab
snsubir1997 <sn.subir1997@outlook.com>
2018-07-04 02:00:09 +0530

    Commit two

--- app/src/main/java/com/example/subir/webservices/MainActivity.java
+++ app/src/main/java/com/example/subir/webservices/MainActivity.java
@@ -53,15 +53,57 @@ public class MainActivity extends AppCompatActivity implements OnWebServiceResul
         }
     }
 
-
     @Override
     public void getWebResponse(String result, CommonUtilities.SERVICE_TYPE type) {
         Log.i("Response IS::","type ="+type+"XML is ::"+result);
 
         try{
             JSONObject obj= new JSONObject(result);
-            String jsonData = obj.toString();
-            jsonPreview.setText(jsonData);
+
+            String response_code = obj.getString("response_code");
+            if(response_code == "200") {
+
+                String date_of_journey = obj.getString("doj");
+                String pnr = obj.getString("pnr");
+
+                JSONObject to_station = obj.getJSONObject("to_station");
+                String boarding_station = to_station.getString("name");
+
+                JSONObject from_station = obj.getJSONObject("to_station");
+                String final_station = from_station.getString("name");
+
+                JSONObject train = obj.getJSONObject("train");
+                String train_name = train.getString("name");
+
+                JSONObject journey_class = obj.getJSONObject("journey_class");
+                String class_code = journey_class.getString("code");
+
+                JSONArray jsonArray = obj.getJSONArray("passengers");
+                String passenger_seat_data = "";
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String no = jsonObject.getString("no");
+                    String booking_status = jsonObject.getString("booking_status");
+                    String current_status = jsonObject.getString("current_status");
+
+                    passenger_seat_data += no + ") Booking Status : " + booking_status + "\n" +
+                            "   Current Status : " + current_status + "\n\n\n";
+                }
+
+                String info = "PNR Number : " + pnr + "\n" +
+                        "Train Name  : " + train_name + "\n" +
+                        "From : " + boarding_station + "\n" +
+                        "To : " + final_station + "\n" +
+                        "Date Of Journey : " + date_of_journey + "\n" +
+                        "Class : " + class_code + "\n\n" +
+                        "Passenger Datails : " + "\n" +
+                        passenger_seat_data;
+                jsonPreview.setText(info);
+            }else {
+                Toast.makeText(getApplicationContext(),
+                        "Cannot Fetch Data. Try Again",Toast.LENGTH_SHORT).show();
+            }
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
